2438c9f161569dcd1fb090f2f85d93157aa58372
NoOkay <enolesamu@gmail.com>
2026-08-24 19:57:42 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "Velocity 💙",     // this is what people actually see
-        type: 1,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
