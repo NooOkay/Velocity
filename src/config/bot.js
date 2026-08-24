18efacb776556ccc024f9d7a184358fc98c7c928
NoOkay <enolesamu@gmail.com>
2026-08-24 20:01:41 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Velocity 💙",     // this is what people actually see
+        name: "Velocity 💙", // required by Discord API, not shown in the client
+        state: "youtube.com/@ClientVelocity",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
